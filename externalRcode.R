3b668ded72bda24a86681c7e7e762020593ec333
Aboli Moroney <aboli.moroney9@gmail.com>
2020-02-08 17:19:16 -0800

    Updated rmd, external code and bibtex

--- externalRcode.R
+++ externalRcode.R
@@ -1,13 +1,8 @@
+# ---- makemyplot
+
 #R script in same directory as RMD file
 xvars <- 1:10
 yvars <- xvars^2
 
-#display xvars
-xvars
-
-#display xvars using print() function
-print(xvars)
-
 #plot
-plot(xvars, yvars)
 plot(xvars, yvars, pch = 16, col = "red", cex = 1.5)
